2e98447f09eb61e906b9f91e761b80c3abd573a5
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-03-06 09:35:44 +0530

    Rabin Karp method implemented.
    Implemented without the modulus part.

---
+++ Rabin_Karp_Method/rabin_karp.c
@@ -0,0 +1,82 @@
+/* Program to implement the Rabin Karp Method */
+
+#include <stdio.h>
+#include <string.h>
+
+#define STRING_LEN 100
+#define PATTERN_LEN 100
+
+#define _HASH_BASE 10
+
+#define _hash(_str, _len, _hash_val)\
+do {\
+  unsigned int i;\
+  _hash_val = 0;\
+  for ( i = 0; i < _len; i++ )\
+  {\
+    _hash_val = _hash_val * _HASH_BASE + _str[i];\
+  }\
+} while (0)
+
+/* TODO : Think of a better name for this function */
+#define _hash_const(_n, _var)\
+{\
+  unsigned int i = _n;\
+  _var = 1;\
+  while ( i-- > 0)\
+  {\
+    _var = _var * _HASH_BASE;\
+  }\
+}
+
+#define _match(_string, _pattern, _len_pattern, _is_match)\
+{\
+  unsigned int tmp = _len_pattern;\
+  while (tmp-- > 0)\
+  {\
+    if (*string == *pattern)\
+      continue;\
+    else\
+      _is_match = 0; break;\
+  }\
+  _is_match = 1;\
+} 
+
+
+#define string_search(_string, _len_string, _pattern, _len_pattern, _ret) \
+do {\
+  unsigned int _pattern_hash, _string_hash; \
+  _hash(_pattern, _len_pattern, _pattern_hash);\
+  _hash(_string,  _len_pattern,  _string_hash);\
+  unsigned int i, _shift_const;\
+  _hash_const (_len_pattern - 1, _shift_const);\
+  char _matches = 0;\
+  _ret = -1;\
+\
+  for ( i = 0; i < _len_string - _len_pattern + 1; i++)\
+  {\
+    if ( i != 0)\
+      _string_hash = (_string_hash - (_string[i - 1] * _shift_const)) * _HASH_BASE + _string[i + _len_pattern - 1];\
+    \
+    if ( _pattern_hash == _string_hash )\
+    {\
+      _match (_string + i, _pattern, _len_pattern, _matches);\
+      if (_matches)\
+        _ret = i; break;\
+    }\
+  }\
+\
+} while(0)
+
+int main (int argc, char *argv[])
+{
+  char string[] = "t123asdfasdfestabc";
+  char pattern[] = "asdfe";
+
+  unsigned int t;
+  string_search(string, strlen(string), pattern, strlen(pattern), t);
+
+  printf ("%d\n", t);
+
+  return 0;
+}
